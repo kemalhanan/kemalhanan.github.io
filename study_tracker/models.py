c9053514ffbacd983dbe7fa1b13b47c5b061560d
Muhammad Kemal Hanan <hananmuhammadkemal@gmail.com>
2023-02-26 12:13:24 +0700

    Membuat model

--- study_tracker/models.py
+++ study_tracker/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Assignment    (models.Model):
+    name = models.CharField(max_length=50)
+    subject = models.CharField(max_length=20)
+    progress = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
--- study_tracker/views.py
+++ study_tracker/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_tracker(request):
+    return render(request, "tracker.html")
