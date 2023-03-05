34252b0521752a8caff5e7751cd1d8f81f20ab59
Muhammad Kemal Hanan <hananmuhammadkemal@gmail.com>
2023-03-05 18:07:46 +0700

    Tugas 3 Sudah Selesai membuat forms

--- study_tracker/urls.py
+++ study_tracker/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from study_tracker.views import show_tracker
+from study_tracker.views import add_assignment
 
 app_name = 'study_tracker'
 
 urlpatterns = [
     path('', show_tracker, name='show_tracker'),
+    path('add_assignment/', add_assignment, name='add_assignment')
 ]
--- study_tracker/views.py
+++ study_tracker/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from study_tracker.models import Assignment
+from django.http import HttpResponseRedirect
+from study_tracker.forms import AssignmentForm
+from django.urls import reverse
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_tracker(request):
-    return render(request, "tracker.html")
+    assignments = Assignment.objects.all()
+    context = {
+        'list_of_assignments': assignments,
+        'name': 'Kemal'
+    }
+    return render(request, 'tracker.html', context)
+
+def add_assignment(request):
+    if request.method == 'POST':
+        form = AssignmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('study_tracker:show_tracker'))
+    else:
+        form = AssignmentForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'add_assignment.html', context)
+
+def show_xml(request):
+    data = Assignment.objects.all()
+    return HttpResponse(serializers.serialize('xml', data), content_type='application/xml')
+
+def show_json(request):
+    data = Assignment.objects.all()
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
