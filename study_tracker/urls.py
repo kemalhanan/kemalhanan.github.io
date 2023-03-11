db652b75510acfcb90251b099d5d5674c1dd944d
Muhammad Kemal Hanan <hananmuhammadkemal@gmail.com>
2023-03-11 13:53:45 +0700

    Tugas 4 membuat registrasi, login/out, restriksi

--- study_tracker/urls.py
+++ study_tracker/urls.py
@@ -3,6 +3,9 @@ from study_tracker.views import show_tracker
 from study_tracker.views import add_assignment
 from study_tracker.views import show_xml
 from study_tracker.views import show_json
+from study_tracker.views import register
+from study_tracker.views import login_user
+from study_tracker.views import logout_user
 
 app_name = 'study_tracker'
 
@@ -11,4 +14,7 @@ urlpatterns = [
     path('add_assignment/', add_assignment, name='add_assignment'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     ]
--- study_tracker/views.py
+++ study_tracker/views.py
@@ -5,13 +5,20 @@ from study_tracker.forms import AssignmentForm
 from django.urls import reverse
 from django.http import HttpResponse
 from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='study_tracker:login')
 def show_tracker(request):
     assignments = Assignment.objects.all()
     context = {
         'list_of_assignments': assignments,
-        'name': 'Kemal'
+        'name': request.user.username,
     }
     return render(request, 'tracker.html', context)
 
@@ -37,3 +44,33 @@ def show_json(request):
     assignments = Assignment.objects.all()
     json = serializers.serialize('json', assignments)
     return HttpResponse(json, content_type='application/json')
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('study_tracker:login')
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('study_tracker:show_tracker')
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('study_tracker:login')
