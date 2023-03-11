1cd646a6d2c068db3492876123e107762c3d54be
Muhammad Kemal Hanan <hananmuhammadkemal@gmail.com>
2023-03-11 14:02:24 +0700

    Tugas 4 Menambahkan cookies

--- study_tracker/views.py
+++ study_tracker/views.py
@@ -11,6 +11,9 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 @login_required(login_url='study_tracker:login')
@@ -19,6 +22,7 @@ def show_tracker(request):
     context = {
         'list_of_assignments': assignments,
         'name': request.user.username,
+        'last_login': request.COOKIES.get('last_login')
     }
     return render(request, 'tracker.html', context)
 
@@ -64,8 +68,10 @@ def login_user(request):
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
-            return redirect('study_tracker:show_tracker')
+            login(request, user) # melakukan login terlebih dahulu
+            response = HttpResponseRedirect(reverse("study_tracker:show_tracker")) # membuat response
+            response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
+            return response       
         else:
             messages.info(request, 'Username atau Password salah!')
     context = {}
@@ -73,4 +79,6 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
+    response = HttpResponseRedirect(reverse('study_tracker:login'))
+    response.delete_cookie('last_login')
     return redirect('study_tracker:login')
